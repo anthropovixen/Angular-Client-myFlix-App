f203a075be4a7b222874d8b45f84e44e841aaff7
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-07-21 14:15:06 +1000

    Create component for user login

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserLoginService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
+})
+export class UserLoginFormComponent implements OnInit {
+  isLoading = false;
+
+  @Input() userData = { Username: '', Password: '' };
+
+  constructor(
+    public fetchApiData: UserLoginService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {}
+
+  logInUser(): void {
+    this.isLoading = true;
+    this.fetchApiData.userLogin(this.userData).subscribe(
+      (result) => {
+        this.isLoading = true;
+        this.dialogRef.close();
+        console.log(result);
+        localStorage.setItem('user', result.user._id);
+        localStorage.setItem('token', result.token);
+
+        this.snackBar.open('user logged in successfully', 'OK', {
+          duration: 2000,
+        });
+      },
+      (result) => {
+        console.log(result);
+        this.snackBar.open(result, 'OK', {
+          duration: 2000,
+        });
+      }
+    );
+  }
+}
